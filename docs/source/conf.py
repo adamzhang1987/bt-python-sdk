a5f55bf8aa18969bef434a3645ad0a409765528b
Adam Zhang <adamzhang1987@gmail.com>
2021-12-26 14:43:38 +0800

    Modified docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/Users/adamzhang/PycharmProjects/bt-python-sdk'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
